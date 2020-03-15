a995c9f1c2049ec156e64301834d07923ae2267a
Developer <qknguyen95@gmail.com>
2020-03-15 21:50:47 +0000

    add font-awesome and complete layout of side menu

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGrinBeam,
+  faStar,
+  faCalendar
+} from "@fortawesome/free-regular-svg-icons";
+import { faTasks, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import "./SideMenu.css";
 
 class SideMenu extends Component {
   render() {
-    return <div className="navigation__body"></div>;
+    return (
+      <div className="side-menu">
+        <div className="side-menu__content">
+          <ul className="side-menu__list">
+            <li>
+              <div className="side-menu__list-item">
+                <div className="side-menu__list-item-content">
+                  <FontAwesomeIcon icon={faGrinBeam} />
+                  <span>My Day</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="side-menu__list-item">
+                <div className="side-menu__list-item-content">
+                  <FontAwesomeIcon icon={faStar} />
+                  <span>Important</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="side-menu__list-item">
+                <div className="side-menu__list-item-content">
+                  <FontAwesomeIcon icon={faCalendar} />
+                  <span>Planned</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="side-menu__list-item">
+                <div className="side-menu__list-item-content">
+                  <FontAwesomeIcon icon={faTasks} />
+                  <span>Tasks</span>
+                </div>
+              </div>
+            </li>
+
+            <hr></hr>
+
+            <li>
+              <div className="side-menu__list-item">
+                <div className="side-menu__list-item-content">
+                  <FontAwesomeIcon icon={faPlus} />
+                  <span>New List</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-ReactDOM.render(
-  <MuiThemeProvider>
-    <HomePage />
-  </MuiThemeProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<HomePage />, document.getElementById("root"));
