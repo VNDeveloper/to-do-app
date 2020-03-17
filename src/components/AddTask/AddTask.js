b0b72a949c8928006330b3bd990d59832f711727
Developer <qknguyen95@gmail.com>
2020-03-17 21:09:03 +0000

    working on add task feature

---
+++ src/components/AddTask/AddTask.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./AddTask.css";
+
+const AddTask = () => {
+  return (
+    <div className="add-task">
+      <div className="add-task__content">
+        <div className="add-task__text">
+          <p>Hello this my first task</p>
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  );
+};
+
+export default AddTask;
