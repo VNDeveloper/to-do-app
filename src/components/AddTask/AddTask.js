f9d68113630a6f463ca962495b292f9bef66a418
Developer <qknguyen95@gmail.com>
2020-03-21 20:57:40 +0000

    add addition feature to task layout and workout how to edit, WIP

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,24 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faStar } from "@fortawesome/free-regular-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import "./AddTask.css";
 
 const Task = () => {
+  let task = false;
+  const [isUserEdit, setUserEdit] = useState(true);
+  const [taskName, setTaskName] = useState("");
+  console.log("isUserEdit", isUserEdit);
+
   return (
     <div className="task">
       <div className="task__container">
         <div className="task__content">
-          <div className="task__content-selected-icon">
-            <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
+          <div
+            className="task__content-selected-icon"
+            onClick={e => setUserEdit(false)}
+          >
+            <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
           </div>
-          <div className="task__content-text">
-            <span>Something else</span>
-            <span>Something</span>
+          <div
+            className="task__content-info"
+            onClick={e => {
+              console.log(e);
+              setUserEdit(true);
+            }}
+            onKeyDown={e => {
+              console.log("onKeyDown e", e);
+              setTaskName("random");
+            }}
+          >
+            <div
+              className="task__content-name"
+              contentEditable={isUserEdit}
+            ></div>
+            <div className="task__content-icons"></div>
           </div>
           <div className="task__content-favorite-icon">
-            <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            {task ? <FontAwesomeIcon icon={faStar}></FontAwesomeIcon> : null}
           </div>
         </div>
       </div>
--- src/components/DetailView/DetailView.js
+++ src/components/DetailView/DetailView.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 import Task from "../AddTask/AddTask";
 import "./DetailView.css";
 
-class DetailView extends Component {
-  render() {
-    return (
-      <div className="detail-view">
-        <div className="detail-view__header">
-          <div className="detail-view__header-content">
-            <h2>{this.props.currentSelectedMenu}</h2>
-            <FontAwesomeIcon icon={faEllipsisH} />
-          </div>
-        </div>
-        <div className="detail-view__task-content">
-          <Task />
+const DetailView = ({ currentSelectedMenu }) => {
+  return (
+    <div className="detail-view">
+      <div className="detail-view__header">
+        <div className="detail-view__header-content">
+          <h2>{currentSelectedMenu}</h2>
+          <FontAwesomeIcon icon={faEllipsisH} />
         </div>
       </div>
-    );
-  }
-}
+      <div className="detail-view__task-content">
+        <Task />
+      </div>
+    </div>
+  );
+};
 
 export default DetailView;
