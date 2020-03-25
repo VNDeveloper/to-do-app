f4c7edfeeff5559137e310acb1236141dc64e09b
Developer <qknguyen95@gmail.com>
2020-03-25 21:27:10 +0000

    add test to project

---
+++ src/components/DetailView/DetailView.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+test("Link changes the class when hovered", () => {
+  expect(true).toBe(true);
+});
+
+test("Link changes the class when hovered", () => {
+  expect(true).toBe(true);
+});
