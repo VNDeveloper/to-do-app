30b601746a81b897a12eab7285d395a702285ef7
Developer <qknguyen95@gmail.com>
2020-03-26 20:40:39 +0000

    uninstall test

--- src/components/DetailView/DetailView.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-test("Link changes the class when hovered", () => {
-  expect(true).toBe(true);
-});
-
-test("Link changes the class when hovered", () => {
-  expect(true).toBe(true);
-});
